b30f50c0f5748f1e9c2a42e2d08133c5ddd7c13b
Tobias654 <tobias.mouraa10@gmail.com>
2022-06-03 17:12:20 -0300

    confirmar

--- js/apps.js
+++ js/apps.js
@@ -4,3 +4,35 @@ let selecionado = pais.options.selectedIndex
 
 console.log(pais.options[selecionado]);
 
+
+const passaporte = document.querySelector("#passaporte");
+
+const padraoPassaporte = new RegExp("[a-zA-Z]{2}[0-9]{12}");
+
+const enviar = document.querySelector("#enviar");
+const validacaopassaporte= document.querySelector("#validar-passaporte");
+
+
+const addValidacaoTexto = () => {
+
+    
+}
+enviar.addEventListener("click", (e)=>{
+e.preventDefault()
+e.stopPropagation()
+validacaopassaporte.classList = "validacao"
+
+
+if(padraoPassaporte.test(passaporte.value)){
+    validacaopassaporte.classList.add("valido")
+    validacaopassaporte.innerHTML = "passaporte válido"
+}
+else{
+    validacaopassaporte.classList.add("invalido")
+    validacaopassaporte.innerHTML = "passaporte inválido"
+
+}
+})
+
+
+
